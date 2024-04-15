ff4e9a9d8e1570b2a042f46f78d41d3500c5b713
Dima <Shilenko.Dmitrii@mail.ru>
2024-04-15 16:26:08 +0500

    tests updated

---
+++ tests/auth_register_login_test.go
@@ -0,0 +1,64 @@
+package tests
+
+import (
+	ssov1 "github.com/PrettyPepeBoy/protos/gen/go/sso"
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"sso/tests/suite"
+	"testing"
+	"time"
+)
+
+const (
+	emptyAppID = 0
+	appID      = 1
+	appSecret  = "test-secret"
+
+	passDefaultLen = 10
+)
+
+func TestRegisterLogin_Login_HappyPath(t *testing.T) {
+	ctx, st := suite.New(t)
+	email := gofakeit.Email()
+	password := randomFakePassword()
+
+	idResp, err := st.AuthClient.Register(ctx, &ssov1.RegisterRequest{
+		Email:    email,
+		Password: password,
+	})
+	require.NoError(t, err)
+	assert.NotEmpty(t, idResp.GetUserId())
+
+	respLogin, err := st.AuthClient.Login(ctx, &ssov1.LoginRequest{
+		Email:    email,
+		Password: password,
+		AppId:    appID,
+	})
+	require.NoError(t, err)
+
+	token := respLogin.GetToken()
+	require.NotEmpty(t, token)
+
+	loginTime := time.Now()
+
+	tokenParsed, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		return []byte(appSecret), nil
+	})
+	require.NoError(t, err)
+
+	claims, ok := tokenParsed.Claims.(jwt.MapClaims)
+	assert.True(t, ok)
+	assert.Equal(t, idResp.GetUserId(), int64(claims["uid"].(float64)))
+	assert.Equal(t, email, claims["email"].(string))
+	assert.Equal(t, appID, int(claims["appId"].(float64)))
+
+	const deltaSeconds = 1
+
+	assert.InDelta(t, loginTime.Add(st.Cfg.TokenTTL).Unix(), claims["exp"].(float64), deltaSeconds)
+}
+
+func randomFakePassword() string {
+	return gofakeit.Password(true, true, true, true, false, passDefaultLen)
+}
